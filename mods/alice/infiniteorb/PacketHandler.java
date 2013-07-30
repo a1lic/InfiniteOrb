face00aa8c3475be37fbae45bc4285dbf9a11c12
EnderAlice- <enderalice@yahoo.com>
2013-07-30 10:15:33 +0900

    Fix changes is not notificated to other players.
    Signed-off-by: EnderAlice- <enderalice@yahoo.com>

--- mods/alice/infiniteorb/PacketHandler.java
+++ mods/alice/infiniteorb/PacketHandler.java
@@ -1,12 +1,17 @@
 package mods.alice.infiniteorb;
 
 import java.io.ByteArrayInputStream;
+import java.io.DataInput;
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 import mods.alice.infiniteorb.tileentity.TileEntityGenerator;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.INetworkManager;
+import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.Packet250CustomPayload;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
@@ -18,11 +23,8 @@ public final class PacketHandler implements IPacketHandler
 	@Override
 	public void onPacketData(INetworkManager manager, Packet250CustomPayload packet, Player player)
 	{
-		ByteArrayInputStream byteIn;
-		DataInputStream streamIn;
-		TileEntity entity;
-		World world;
-		int num, nums[];
+		InputStream byteIn;
+		DataInput streamIn;
 
 		if(packet.channel.equals("INFORB__"))
 		{
@@ -31,31 +33,71 @@ public final class PacketHandler implements IPacketHandler
 
 			try
 			{
-				num = streamIn.readByte();
-				if(num == 0)
-				{
-					// Update notification from Generator GUI.
-					nums = new int[6];
-
-					for(byte i = 0; i < 6; i++)
-					{
-						nums[i] = streamIn.readInt();
-					}
-
-					world = ((EntityPlayer)player).worldObj;
-
-					entity = world.getBlockTileEntity(nums[0], nums[1], nums[2]);
-					if(entity != null)
-					{
-						if(entity instanceof TileEntityGenerator)
-						{
-							((TileEntityGenerator)entity).updateParameters(nums[3], nums[4], nums[5]);
-						}
-					}
-				}
+				doInfiniteOrb(streamIn, (EntityPlayer)player);
 			}
 			catch(IOException e)
 			{
+			}
+		}
+	}
+
+	private static void doInfiniteOrb(DataInput stream, EntityPlayer player) throws IOException
+	{
+		Packet pkt;
+		TileEntity entity;
+		World world;
+		int num, nums[];
+
+		num = stream.readByte();
+		if(num == 0)
+		{
+			// Update notification from Generator GUI.
+			nums = new int[6];
+
+			for(byte i = 0; i < 6; i++)
+			{
+				nums[i] = stream.readInt();
+			}
+
+			world = ((EntityPlayer)player).worldObj;
+
+			entity = world.getBlockTileEntity(nums[0], nums[1], nums[2]);
+			if(entity == null)
+			{
+				return;
+			}
+
+			if(!(entity instanceof TileEntityGenerator))
+			{
+				return;
+			}
+
+			((TileEntityGenerator)entity).updateParameters(nums[3], nums[4], nums[5]);
+
+			if(!(player instanceof EntityPlayerMP))
+			{
+				return;
+			}
+
+			pkt = entity.getDescriptionPacket();
+
+			if(pkt != null)
+			{
+				@SuppressWarnings("unchecked")
+				List<EntityPlayer> playerList = world.playerEntities;
+
+				for(EntityPlayer p : playerList)
+				{
+					if(p == player)
+					{
+						continue;
+					}
+
+					if(p instanceof EntityPlayerMP)
+					{
+						((EntityPlayerMP)p).playerNetServerHandler.sendPacketToPlayer(pkt);
+					}
+				}
 			}
 		}
 	}
